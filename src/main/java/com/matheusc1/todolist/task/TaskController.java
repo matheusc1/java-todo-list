f1b054529404cf413a017520634e0a56e88605b2
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2023-10-12 17:50:29 -0300

    Criando método para atualizar a task

--- src/main/java/com/matheusc1/todolist/task/TaskController.java
+++ src/main/java/com/matheusc1/todolist/task/TaskController.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheusc1.todolist.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -47,5 +51,13 @@ public class TaskController {
 
     var tasks = this.taskRepository.findByUserId((UUID) userId);
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    var task = this.taskRepository.findById(id).orElse(null);
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
