c3a9db4f08c14b3777c500310faee474b297340c
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2023-10-13 16:59:16 -0300

    Tratando possíveis erros e criando validações

--- src/main/java/com/matheusc1/todolist/task/TaskController.java
+++ src/main/java/com/matheusc1/todolist/task/TaskController.java
@@ -54,10 +54,22 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var task = this.taskRepository.findById(id).orElse(null);
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+      .body("Tarefa não encontrada.");
+    }
+
+    var userId = request.getAttribute("userId");
+    if (!task.getUserId().equals(userId)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+      .body("Usuário sem permissão para alterar essa tarefa!");
+    }
+
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var updatedTask = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(updatedTask);
   }
 }
